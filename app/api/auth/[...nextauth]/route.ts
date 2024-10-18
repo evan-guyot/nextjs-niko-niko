5bdb77f33f68bb551fe2238d592a94acbaa09c71
Evan GUYOT <evan.guyot2022@campus-eni.fr>
2024-10-18 09:26:10 +0200

    Separating authOptions from auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,55 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-
-const prisma = new PrismaClient();
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID || "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-    }),
-  ],
-  callbacks: {
-    async session({ session, token }) {
-      if (session?.user && token.sub) {
-        session.user.id = token.sub;
-      }
-      return session;
-    },
-
-    async signIn({ user, account, profile }) {
-      const { email, image } = user;
-      if (!email) return false;
-
-      try {
-        const existingUser = await prisma.user.findUnique({
-          where: { email },
-        });
-
-        if (existingUser) {
-          await prisma.user.update({
-            where: { email },
-            data: { lastLoggedIn: new Date() },
-          });
-        } else {
-          await prisma.user.create({
-            data: {
-              email,
-              image,
-              lastLoggedIn: new Date(),
-            },
-          });
-        }
-        return true;
-      } catch (error) {
-        console.error("Error signing in:", error);
-        return false;
-      }
-    },
-  },
-};
+import { authOptions } from "@/types/authOptions";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
